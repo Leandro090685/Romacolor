1d97e5cf902e9b5a93f3315abbdf8a7060477b8f
Leandro090685 <lenife@hotmail.com>
2023-03-10 18:23:24 -0300

    second commit

--- estamperia/estamperia/settings.py
+++ estamperia/estamperia/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e$+!x-2fljv99o$*g_&(+!i%#4$r%w44(nrzvq4zgzm$7sa&0i"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
